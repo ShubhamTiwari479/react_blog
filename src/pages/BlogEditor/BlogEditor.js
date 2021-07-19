153186ae3b94d980258499fb69f575ecdd7a431a
Shubham singh <proshubham5@gmail.com>
2021-07-19 12:56:31 +0530

    visibility api test

--- src/pages/BlogEditor/BlogEditor.js
+++ src/pages/BlogEditor/BlogEditor.js
@@ -1,10 +1,63 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
 export default function BlogEditor(props) {
 
     const [quillDelta, setQuillDelta] = useState("")
+
+        useEffect(() => {
+            onVisibilityChange((visible) => {
+                console.log(`the page is now${visible ? 'focused' : 'unfocused'}` )
+            })
+            }, [])
+
+
+    function onVisibilityChange(callback) {
+        var visible = true;
+
+        if (!callback) {
+            throw new Error('no callback given');
+        }
+
+        function focused() {
+            if (!visible) {
+                callback(visible = true);
+            }
+        }
+
+        function unfocused() {
+            if (visible) {
+                callback(visible = false);
+            }
+        }
+
+        // Standards:
+        if ('hidden' in document) {
+            document.addEventListener('visibilitychange',
+                function() {(document.hidden ? unfocused : focused)()});
+        }
+        if ('mozHidden' in document) {
+            document.addEventListener('mozvisibilitychange',
+                function() {(document.mozHidden ? unfocused : focused)()});
+        }
+        if ('webkitHidden' in document) {
+            document.addEventListener('webkitvisibilitychange',
+                function() {(document.webkitHidden ? unfocused : focused)()});
+        }
+        if ('msHidden' in document) {
+            document.addEventListener('msvisibilitychange',
+                function() {(document.msHidden ? unfocused : focused)()});
+        }
+        // IE 9 and lower:
+        if ('onfocusin' in document) {
+            document.onfocusin = focused;
+            document.onfocusout = unfocused;
+        }
+        // All others:
+        window.onpageshow = window.onfocus = focused;
+        window.onpagehide = window.onblur = unfocused;
+    };
 
     function handleChange(content, delta, source, editor) {
         setQuillDelta(editor.getContents())
