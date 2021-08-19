17512236cc00c81df05f8781900eff1d0bbd7fc4
Shubham singh <proshubham5@gmail.com>
2021-08-19 10:50:50 +0530

    blog post problem solved

--- src/pages/BlogPost/BlogPost.js
+++ src/pages/BlogPost/BlogPost.js
@@ -1,37 +1,39 @@
-import { PostAdd } from "@material-ui/icons";
+
 import axios from "axios";
-import React,{useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import ReactQuill from "react-quill";
 import "./_blogPost.scss"
-
-
-
-
-
+import Quill from 'quill'
+const Delta = Quill.import('delta')
 
 export default function BlogPost() {
 
-    const [post, getPost] = useState()
+    const [post, setPost] = useState(new Delta())
+    const quillRef = useRef()
 
     useEffect(() => {
+        console.log("useeffect 1 running")
         axios.get('https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/todo-zmphz/service/blog/incoming_webhook/blog_get')
-        .then(resposne => {getPost(resposne.data);
-        })
-        
-       
-       
-    }, [getPost])
-       
+        .then(response => {
+            console.log(response.data)
 
+            setPost(new Delta(response.data[38].ops))
 
-    console.log(post);
+        }).catch((e) => {
+                console.error("error came while fetching", e)
+            })
+    }, [])
+
+    console.log("post",post)
 
     return (
         <div style={{marginRight: 10, marginLeft: 10, marginTop: 20}} id={"blog-post-container"}>
-            <ReactQuill Value= {post}
-                        theme="snow"
-                        readOnly={true}
-                        modules={{toolbar: false}}
+            <ReactQuill
+                ref={quillRef}
+                value={post}
+                theme="snow"
+                readOnly={true}
+                modules={{toolbar: false}}
                         
             />
         </div>
